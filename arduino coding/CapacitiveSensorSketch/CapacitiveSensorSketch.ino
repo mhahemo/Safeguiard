829fc74ab85da26a78cfd2b64dbec345b263028d
mhahemo <56228770+mhahemo@users.noreply.github.com>
2019-10-19 21:37:19 -0600

    Add files via upload

---
+++ arduino coding/CapacitiveSensorSketch/CapacitiveSensorSketch.ino
@@ -0,0 +1,34 @@
+#include <CapacitiveSensor.h>
+CapacitiveSensor Sensor = CapacitiveSensor(4, 6);
+long val;
+int pos;
+#define led 13
+
+void setup()
+{
+Serial.begin(9600);
+pinMode(led, OUTPUT);
+Serial.println("resetting...");
+}
+
+void loop()
+{
+
+val = Sensor.capacitiveSensor(30);
+Serial.println(val);
+if (val >= 1000 && pos == 0)
+{
+digitalWrite(led, HIGH);
+pos = 1;
+delay(500);
+}
+
+else if (val >= 1000 && pos == 1)
+{
+digitalWrite(led, LOW);
+pos = 0;
+delay(500);
+}
+
+delay(10);
+}
---
+++ arduino coding/DHT_reading/DHT_reading.ino
@@ -0,0 +1,30 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+  int chk = DHT.read11(DHT11_PIN);
+  if(DHT.humidity > 1)
+  {
+  Serial.print("\nHumidity = ");
+  Serial.println(DHT.humidity);
+    if(DHT.humidity > 70)
+  {
+  Serial.println("co-ordinate");
+  Serial.print("23.815025,90.427337\n");
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("\nHumidity = ");
+  Serial.println(DHT.humidity);
+  delay(1000);
+  }
+}
+  }
